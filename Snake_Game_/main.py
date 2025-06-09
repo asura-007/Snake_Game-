f5634becbca16fbd2e9aa701f9ab3f41cbf5cade
asura-007 <134699166+asura-007@users.noreply.github.com>
2025-06-09 15:36:46 +0530

    Add files via upload

---
+++ Snake_Game_/main.py
@@ -0,0 +1,58 @@
+from tkinter import Image
+from turtle import Screen
+from snake import Snake
+from food import Food
+from Score import Scoreboard
+import time
+from PIL import Image
+
+img = Image.open("bck.jpg")
+img.save("bck.gif")
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.bgpic("bck.gif")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    #Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    #Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    #Detect collision with tail.
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
+
+
+
+
+screen.exitonclick()
